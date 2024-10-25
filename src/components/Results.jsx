372a826ffc1f019a8e1b19c9deb31503df779352
Anas Ahmed <anasahmed700@gmail.com>
2024-10-25 17:00:35 +0500

    Outputting Results in a List & Deriving Values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,35 @@
-import { calculateInvestmentResults } from "../util/investment"
+import { calculateInvestmentResults, formatter } from "../util/investment"
 
 const Results = ({ input }) => {
     const resultsData = calculateInvestmentResults(input); 
-    console.log(resultsData)
+    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
     return (
-        <div>Results</div>
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Investment Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map((yearResult) => {
+                    const totalInterest = yearResult.valueEndOfYear - yearResult.annualInvestment * yearResult.year - initialInvestment;
+                    const totalAmountInvested = yearResult.valueEndOfYear - totalInterest;
+                    return (
+                        <tr key={yearResult.year}>
+                            <td>{yearResult.year}</td>
+                            <td>{formatter.format(yearResult.valueEndOfYear)}</td>
+                            <td>{formatter.format(yearResult.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
+                        </tr>
+                        )
+                })}
+            </tbody>
+        </table>
     )
 }
 
